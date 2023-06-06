9f60099e7453648d9c50eab8d6d60ada65d6b6f9
Catarina Canelas <up202103628@fe.up.pt>
2023-06-06 17:14:00 +0100

    final changes

--- includes/TriangularApprox.h
+++ includes/TriangularApprox.h
@@ -12,20 +12,20 @@
 #include <unordered_set>
 #include "../includes/Graph.h"
 
+using namespace std;
+
 class TriangularApprox {
 public:
     TriangularApprox(Graph g);
-    std::vector<int> triangularApproximationTSP(const std::vector<Vertex *>& nodes);
+    vector<int> triangularApproximationTSP(const vector<Vertex *>& nodes);
 
 protected:
     Graph graph;
-    int findNearestNeighbor(const std::vector<Vertex *>& nodes, int currentNodeIndex);
-    void preorderWalk(int u, std::vector<bool> &visited, std::vector<int> &tour);
+    vector<int> preorderWalk(int u, vector<int> &tour, Graph& g);
     void dfs(int v);
     double haversine(double lat1, double lon1, double lat2, double lon2);
     int minKey(int key[], bool mstSet[]);
     Graph prim();
-    void printMST(int parent[]);
 };
 
 #endif //DA_PROJ2_TRIANGULARAPPROX_H
--- src/TriangularApprox.cpp
+++ src/TriangularApprox.cpp
@@ -1,42 +1,52 @@
-//
-// Created by catar on 01/06/2023.
-//
+
 
 #include "../includes/TriangularApprox.h"
-#include <sstream>
 #include <string>
 #include <random>
-#include <ctime>
-#include <iomanip>
 
 using namespace std;
 
 TriangularApprox::TriangularApprox(Graph g) : graph(g)  {}
 
-void TriangularApprox::preorderWalk(int u, std::vector<bool> &visited, std::vector<int> &tour)
-{
-    visited[u] = true;
-    tour.push_back(u);
+/**
+ * PreOrder walk
+ * It was supposed to iterate the mst in a preorder, to create the tour with the vertices of MST in preorder
+ * @param u
+ * @param tour
+ * @param g
+ * @return tour
+ */
+vector<int> TriangularApprox::preorderWalk(int u, vector<int> &tour, Graph& g){
+    g.getVertexSet()[u]->setVisited(true);
 
-    for (int v = 0; v < graph.getNumVertex(); v++) {
-        if (graph.getVertexSet()[v]->hasEdge(u) && !visited[v])
-            preorderWalk(v, visited, tour);
+    for (int v = 0; v < g.getNumVertex(); v++) {
+        if (g.getVertexSet()[v]->hasEdge(u) && !(g.getVertexSet()[v]->isVisited())) {
+            tour.push_back(g.getVertexSet()[v]->getId());
+            preorderWalk(v, tour, g);
+        }
     }
+
+    return tour;
 }
 
+/**
+ * Haversine Formula, calculates the distance between two vertices with their coordinates
+ * @param lat1
+ * @param lon1
+ * @param lat2
+ * @param lon2
+ * @return distance between two vertices
+ */
 double TriangularApprox::haversine(double lat1, double lon1, double lat2, double lon2){
-    // distance between latitudes
-    // and longitudes
+
     double dLat = (lat2 - lat1) *
                   M_PI / 180.0;
     double dLon = (lon2 - lon1) *
                   M_PI / 180.0;
 
-    // convert to radians
     lat1 = (lat1) * M_PI / 180.0;
     lat2 = (lat2) * M_PI / 180.0;
 
-    // apply formulae
     double a = pow(sin(dLat / 2), 2) +
                pow(sin(dLon / 2), 2) *
                cos(lat1) * cos(lat2);
@@ -45,58 +55,11 @@ double TriangularApprox::haversine(double lat1, double lon1, double lat2, double
     return rad * c;
 }
 
-int TriangularApprox::findNearestNeighbor(const std::vector<Vertex *>& nodes, int currentNodeIndex) {
-//    int nearestNeighbor = -1;
-//    double minDistance = std::numeric_limits<double>::max();
-//
-//    for (int i = 0; i < nodes.size(); ++i) {
-//        if (i != currentNodeIndex && !nodes[i]->isVisited()) {
-//            double distance = ::haversine(nodes[currentNodeIndex]->getLatitude(),nodes[currentNodeIndex]->getLongitude() , nodes[i]->getLatitude(),nodes[i]->getLongitude() );
-//            if (distance < minDistance) {
-//                minDistance = distance;
-//                nearestNeighbor = i;
-//            }
-//        }
-//    }
-//
-//    return nearestNeighbor;
-}
 
-std::vector<int> TriangularApprox::triangularApproximationTSP(const std::vector<Vertex *>& nodes) {
-//    int numNodes = nodes.size();
-    std::vector<int> tour;
-//    tour.reserve(numNodes);
-//
-//    // Start with the first node as the current node
-//    int currentNodeIndex = 0;
-//    nodes[currentNodeIndex]->setVisited(true);
-//    tour.push_back(nodes[currentNodeIndex]->getId());
-//
-//    while (tour.size() < numNodes) {
-//        int nearestNeighborIndex = findNearestNeighbor(nodes, currentNodeIndex);
-//        nodes[nearestNeighborIndex]->setVisited(true);
-//        tour.push_back(nodes[nearestNeighborIndex]->getId());
-//        currentNodeIndex = nearestNeighborIndex;
-//    }
-//
-//    // Connect back to the starting node
-//    tour.push_back(nodes[0]->getId());
-//
-//    std::vector<bool> visited(numNodes, false);
-//    std::vector<int> preorderTour;
-//    preorderTour.reserve(numNodes);
-//    preorderWalk(0, visited, preorderTour);
-//
-//    return preorderTour;
-    dfs(0);
-    Graph g = prim();
-//    for(Vertex *v : g.getVertexSet()){
-//        cout << v->getId();
-//    }
-
-    return tour;
-}
-
+/**
+ * dfs to calculate the distances between all vertices with edges with haversine formula
+ * @param v
+ */
 void TriangularApprox::dfs(int v){
     Vertex *s = graph.findVertex(v);
     s->setVisited(true);
@@ -111,54 +74,80 @@ void TriangularApprox::dfs(int v){
     }
 }
 
-void TriangularApprox::printMST(int parent[])
-{
-    cout << "Edge \tWeight\n";
-    for (int i = 1; i < graph.getNumVertex(); i++) {
-        Vertex *aux = graph.findVertex(i);
-        Vertex *to = graph.findVertex(parent[i]);
-        cout << parent[i] << " - " << i << " \t" << aux->getEdgeWeight(to->getId()) << " \n";
-    }
-}
 
-int TriangularApprox::minKey(int key[], bool mstSet[]){
+/**
+ * Iterates trough the vertices of the graph and verifies if a specific vertice is in the MST and if it has a key value inferior to the
+ * key value actual. If these conditions are met, updated the minimum value of key.
+ * Purpose: to find the vertice with the smallest value key of the vertices not yet included in MST
+ * @param key -> vector to find the edge with smallest distance
+ * @param mst -> minimum spanning tree
+ * @return
+ */
+int TriangularApprox::minKey(int key[], bool mst[]){
     int min = INT_MAX, min_index;
 
-    for (int v = 0; v < graph.getNumVertex(); v++)
-        if (!mstSet[v] && key[v] < min)
+    for (int v = 0; v < graph.getNumVertex(); v++) {
+        if (!mst[v] && key[v] < min) {
             min = key[v], min_index = v;
-
+        }
+    }
     return min_index;
 }
-
+/**
+ * Implementation of prim algorithm to construct a MST (minimum spanning tree) which has the minimum value of distance of the edge
+ * those vertices with the minimum value for the edges are added to the mst
+ * @return graph g
+ */
 Graph TriangularApprox::prim(){
     Graph g;
     int key[graph.getNumVertex()];
 
-    bool mstSet[graph.getNumVertex()];
+    bool mst[graph.getNumVertex()];
 
-    for (int i = 0; i < graph.getNumVertex(); i++)
-        key[i] = INT_MAX, mstSet[i] = false;
+    for (int i = 0; i < graph.getNumVertex(); i++) {
+        key[i] = INT_MAX;
+        mst[i] = false;
+    }
 
     key[0] = 0;
 
-//    g.addVertex(-1);
 
-    for (int count = 0; count < graph.getNumVertex() - 1; count++) {
-        int u = minKey(key, mstSet);
+    for (int i = 0; i < graph.getNumVertex() - 1; i++) {
+        int u = minKey(key, mst);
 
-        mstSet[u] = true;
+        mst[u] = true;
         Vertex *a = graph.findVertex(u);
         g.addVertex(a->getId());
         for (int v = 0; v < graph.getNumVertex(); v++) {
             Vertex *b = graph.findVertex(v);
-            if (a->getEdgeWeight(b->getId()) != 0 && !mstSet[v] && a->getEdgeWeight(b->getId()) < key[v]) {
+            if (a->getEdgeWeight(b->getId()) != 0 && !mst[v] && a->getEdgeWeight(b->getId()) < key[v]) {
                 g.addEdge(a->getId(), b->getId(), a->getEdgeWeight(b->getId()));
-                //parent[v] = u, key[v] = a->getEdgeWeight(b->getId());
+                key[v] = a->getEdgeWeight(b->getId());
             }
         }
     }
     return g;
-    //printMST(parent);
 
+}
+
+
+/**
+ * Algorithm which calculates the result
+ * 1st -> calculates distances of all vertices with their coordenates
+ * 2st -> constructs the MST
+ * 3st -> it was supposed to iterare the MST in preorder to create the vector tour with the vertices in the MST
+ * @param nodes
+ * @return tour, vector with the vertices of MST
+ */
+vector<int> TriangularApprox::triangularApproximationTSP(const vector<Vertex *>& nodes) {
+    vector<int> tour;
+    dfs(0);
+    Graph g = prim();
+    //tour = preorderWalk(0, tour, g);
+    for(Vertex *v : g.getVertexSet()){
+        tour.push_back(v->getId());
+    }
+    tour.push_back(0);
+
+    return tour;
 }
